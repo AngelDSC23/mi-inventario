06aa5af41783404e55948000166154fbec7cacc3
Ángel <angelserranochamba@gmail.com>
2025-10-16 15:06:57 +0200

    auto-update: 15:06:57

--- src/App.tsx
+++ src/App.tsx
@@ -231,7 +231,7 @@ export default function App() {
             currentSection={currentSection}
             selectedFieldIndex={null}
             setSelectedFieldIndex={() => {}}
-            addField={() => {}}
+            addField={addField}
             deleteField={() => {}}
             moveField={() => {}}
             updateField={() => {}}
